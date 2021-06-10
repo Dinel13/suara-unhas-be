31b494db1919b994608cc739309acfcefd7518cf
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-06-10 19:25:33 +0800

    try github action

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require("cors");
 
 const userRoute = require("./routes/user-route");
 const categoryRoutes = require("./routes/category");
@@ -12,11 +12,10 @@ const HttpError = require("./models/http-error");
 
 const app = express();
 
-
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 
-app.use(cors('*'))
+app.use(cors("*"));
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
@@ -31,9 +30,9 @@ app.use((req, res, next) => {
 });
 
 app.use("/api", userRoute);
-app.use('/api', categoryRoutes);
-app.use('/api', tagRoutes);
-app.use('/api', blogRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", tagRoutes);
+app.use("/api", blogRoutes);
 
 //midleware sebagai defaul jika route tidak ditemukan
 app.use((req, res, next) => {
@@ -50,16 +49,11 @@ app.use((error, req, res, next) => {
 
 const url = "mongodb://127.0.0.1:27017/unhasta";
 mongoose
-  .connect(
-    url,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-    //  `mongodb+srv://${process.env.MONGGODB_USER_PASSWORD}@cluster0.enucz.mongodb.net/genbi?retryWrites=true&w=majority`
-  )
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((result) => {
     app.listen(process.env.PORT || 8080);
   })
   .catch((err) => console.log(err));
-
